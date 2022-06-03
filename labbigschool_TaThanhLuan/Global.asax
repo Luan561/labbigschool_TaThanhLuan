892edd33fd94cf29b469f597c6df94732b4d1ccb
luan <luan@LAPTOP-TN5QSCPF>
2022-06-03 16:03:02 +0700

    Add project files.

---
+++ labbigschool_TaThanhLuan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="labbigschool_TaThanhLuan.MvcApplication" Language="C#" %>
---
+++ labbigschool_TaThanhLuan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(labbigschool_TaThanhLuan.Startup))]
+namespace labbigschool_TaThanhLuan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
